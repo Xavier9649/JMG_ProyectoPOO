3f79659803648f7e8cf96e7cff2d2e95d5f1bacb
Miguel <akmp2019@outlook.com>
2025-07-24 13:02:13 -0500

    CREACION login

---
+++ src/LOGIN.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class LOGIN {
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JComboBox comboBox1;
+    private JButton INGRESARButton;
+}
