3d5f9fcc43e62f087269137c4dbc6528392227f9
Miguel <akmp2019@outlook.com>
2025-07-24 20:17:15 -0500

    CREACION login y administrador

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,27 +2,28 @@ public class Cliente {
     private int id_cliente;
     private String nombre;
     private String apellido;
-    private int cedulaCliente;
     private int telefono;
     private String correo;
+    private String direccion;
 
     //Constructor
-    public Cliente (int id_cliente, String nombre, String apellido, int cedulaCliente, int telefono, String correo) {
+    public Cliente (int id_cliente, String nombre, String apellido, int cedulaCliente, int telefono, String correo, String direccion) {
         this.id_cliente = id_cliente;
         this.nombre = nombre;
         this.apellido = apellido;
-        this.cedulaCliente = cedulaCliente;
         this.telefono = telefono;
         this.correo = correo;
+        this.direccion=direccion;
     }
 
     //Metodos
     //Registrar Cliente
     public void registrarCliente() {
         System.out.println("Cliente Registrado: " + nombre + " " + apellido);
-        System.out.println("Cédula: " + cedulaCliente);
+        System.out.println("Cédula: " + id_cliente);
         System.out.println("Teléfono: " + telefono);
         System.out.println("Correo: " + correo);
+        System.out.println("Correo: " + direccion);
     }
     //Getters
     public int getIdCliente() {
@@ -34,9 +35,7 @@ public class Cliente {
     public String getApellido() {
         return apellido;
     }
-    public int getCedulaCliente() {
-        return cedulaCliente;
-    }
+
     public int getTelefono() { return telefono; }
     public String getCorreo() { return correo; }
 }
